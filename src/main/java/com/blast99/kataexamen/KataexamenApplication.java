86ec9036e340862414d13d18b973b20128ebca67
Juan Carlos Rodriguez <j.rodriguez.arrieta@accenture.com>
2018-02-21 20:06:01 -0600

    Initial commit | base project

---
+++ src/main/java/com/blast99/kataexamen/KataexamenApplication.java
@@ -0,0 +1,12 @@
+package com.blast99.kataexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataexamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataexamenApplication.class, args);
+	}
+}
---
+++ src/test/java/com/blast99/kataexamen/KataexamenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blast99.kataexamen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KataexamenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
