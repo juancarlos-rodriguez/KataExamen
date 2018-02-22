bd357be7403dcf2aa852d8faae8521aaffd68407
Juan Carlos Rodriguez <j.rodriguez.arrieta@accenture.com>
2018-02-21 22:48:19 -0600

    REFACTOR: Se separaron los tests por cada método y se agregaron los tests de los Exceptions, se optimizó el código de la clase ProcesadorRomano

--- src/main/java/com/blast99/kataexamen/ProcesadorRomano.java
+++ src/main/java/com/blast99/kataexamen/ProcesadorRomano.java
@@ -4,72 +4,27 @@ public class ProcesadorRomano {
 
     public int convierte(String texto) throws Exception {
 
+        if (texto.equals("")) { return 0; }
+
         int resultado = 0;
-        char valor;
 
-        if (texto.equals("")) {
-            return 0;
-        }
+        if (!cantidadAceptableChar(texto)) throw new Exception("El texto no tiene la cantidad aceptable de caracteres");
 
-        if (!cantidadAceptableChar(texto)) {
-            throw new Exception("El texto no tiene la cantidad aceptable de caracteres");
-        }
-
-        if (!ordenDescendiente(texto)) {
-            throw new Exception("El texto no se encuentra de forma descendente");
-        }
-
+        if (!ordenDescendiente(texto)) throw new Exception("El texto no se encuentra de forma descendiente");
 
         for (int i = 0; i < texto.length(); i++) {
-
-            valor = texto.charAt(i);
-            switch (valor) {
-                case 'I':
-                    resultado += 1;
-                    break;
-
-                case 'V':
-                    resultado += 5;
-                    break;
-
-                case 'X':
-                    resultado += 10;
-                    break;
-
-                case 'L':
-                    resultado += 50;
-                    break;
-
-                case 'C':
-                    resultado += 100;
-                    break;
-
-                case 'D':
-                    resultado += 500;
-                    break;
-
-                case 'M':
-                    resultado += 1000;
-                    break;
-
-                default:
-                    resultado = 0;
-                    break;
-            }
+            resultado += retornaValorUnidad(texto.charAt(i));
         }
 
         return resultado;
-
     }
 
     public boolean cantidadAceptableChar(String texto) {
 
         int cantidadI = 0, cantidadL = 0, cantidadD = 0, cantidadX = 0, cantidadC = 0;
-        char c;
 
         for (int i = 0; i < texto.length(); i++) {
-            c = texto.charAt(i);
-            switch (c) {
+            switch (texto.charAt(i)) {
                 case 'I':
                     cantidadI++;
                     break;
@@ -89,113 +44,63 @@ public class ProcesadorRomano {
                 case 'D':
                     cantidadD++;
                     break;
-
             }
 
-            if (cantidadI > 3 || cantidadL > 1 || cantidadD > 1 || cantidadX > 3 || cantidadC > 3) {
-                return false;
-            }
+            if (cantidadI > 3 || cantidadL > 1 || cantidadD > 1 || cantidadX > 3 || cantidadC > 3) return false;
+
         }
         return true;
     }
 
-    public static boolean ordenDescendiente(String texto) {
-        int actual;
-        int siguiente;
-        char valorActual;
-        char valorSiguiente;
+    public boolean ordenDescendiente(String texto) {
 
         if (texto.length() >= 2) {
             for (int i = 0; i < texto.length(); i++) {
-
-                valorActual = texto.charAt(i);
-                switch (valorActual) {
-                    case 'I':
-                        actual = 1;
-                        break;
-
-                    case 'V':
-                        actual = 5;
-                        break;
-
-                    case 'X':
-                        actual = 10;
-                        break;
-
-                    case 'L':
-                        actual = 50;
-                        break;
-
-                    case 'C':
-                        actual = 100;
-                        break;
-
-                    case 'D':
-                        actual = 500;
-                        break;
-
-                    case 'M':
-                        actual = 1000;
-                        break;
-
-                    default:
-                        actual = 0;
-                        break;
-                }
-
-
-
-
-
-                ////
-//                actual = texto.charAt(i);
-                if ((i + 1) == texto.length()) {
-                    return true;
-                }
-//                siguiente = texto.charAt(i+1);
-
-                valorSiguiente = texto.charAt(i+1);
-                switch (valorSiguiente) {
-                    case 'I':
-                        siguiente = 1;
-                        break;
-
-                    case 'V':
-                        siguiente = 5;
-                        break;
-
-                    case 'X':
-                        siguiente = 10;
-                        break;
-
-                    case 'L':
-                        siguiente = 50;
-                        break;
-
-                    case 'C':
-                        siguiente = 100;
-                        break;
-
-                    case 'D':
-                        siguiente = 500;
-                        break;
-
-                    case 'M':
-                        siguiente = 1000;
-                        break;
-
-                    default:
-                        siguiente = 0;
-                        break;
-                }
-
-                if (actual < siguiente) {
-                    return false;
-                }
+                if ((i + 1) == texto.length()) return true;
+                if (retornaValorUnidad(texto.charAt(i)) < retornaValorUnidad(texto.charAt(i+1))) return false;
             }
         }
 
         return true;
+    }
 
+    public int retornaValorUnidad(char c) {
+        int result;
+
+        switch (c) {
+            case 'I':
+                result = 1;
+                break;
+
+            case 'V':
+                result = 5;
+                break;
+
+            case 'X':
+                result = 10;
+                break;
+
+            case 'L':
+                result = 50;
+                break;
+
+            case 'C':
+                result = 100;
+                break;
+
+            case 'D':
+                result = 500;
+                break;
+
+            case 'M':
+                result = 1000;
+                break;
+
+            default:
+                result = 0;
+                break;
+        }
+
+        return result;
     }
 }
--- src/test/java/com/blast99/kataexamen/ProcesadorRomanoTests.java
+++ src/test/java/com/blast99/kataexamen/ProcesadorRomanoTests.java
@@ -10,9 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ProcesadorRomanoTests {
 
+    private ProcesadorRomano instance = new ProcesadorRomano();
+
     @Test
     public void testConvierte() throws Exception {
-        ProcesadorRomano instance = new ProcesadorRomano();
 
         Assert.assertEquals(0, instance.convierte(""));
         Assert.assertEquals(1, instance.convierte("I"));
@@ -26,6 +27,17 @@ public class ProcesadorRomanoTests {
         Assert.assertEquals(1000, instance.convierte("M"));
         Assert.assertEquals(0, instance.convierte("T"));
 
+        Assert.assertEquals(60, instance.convierte("LX"));
+        Assert.assertEquals(16, instance.convierte("XVI"));
+        Assert.assertEquals(8, instance.convierte("VIII"));
+        Assert.assertEquals(21, instance.convierte("XXI"));
+        Assert.assertEquals(101, instance.convierte("CI"));
+        Assert.assertEquals(161, instance.convierte("CLXI"));
+
+    }
+
+    @Test
+    public void testCantidadAceptableChar() {
 
         Assert.assertTrue(instance.cantidadAceptableChar("III"));
         Assert.assertFalse(instance.cantidadAceptableChar("IIII"));
@@ -38,18 +50,46 @@ public class ProcesadorRomanoTests {
         Assert.assertTrue(instance.cantidadAceptableChar("CCC"));
         Assert.assertFalse(instance.cantidadAceptableChar("CCCC"));
 
+    }
+
+    @Test
+    public void testOrdenDescendiente() {
 
         Assert.assertFalse(instance.ordenDescendiente("XXL"));
         Assert.assertTrue(instance.ordenDescendiente("LXX"));
         Assert.assertTrue(instance.ordenDescendiente("X"));
         Assert.assertTrue(instance.ordenDescendiente(""));
+        Assert.assertTrue(instance.ordenDescendiente("LX"));
+        Assert.assertTrue(instance.ordenDescendiente("XVI"));
+        Assert.assertTrue(instance.ordenDescendiente("VIII"));
+        Assert.assertTrue(instance.ordenDescendiente("XXI"));
+        Assert.assertTrue(instance.ordenDescendiente("CI"));
+        Assert.assertTrue(instance.ordenDescendiente("CLXI"));
+    }
 
+    @Test
+    public void testRetornaValorUnidad() {
 
-        Assert.assertEquals(60, instance.convierte("LX"));
-        Assert.assertEquals(16, instance.convierte("XVI"));
-
-
+        Assert.assertEquals(1, instance.retornaValorUnidad('I'));
+        Assert.assertEquals(5, instance.retornaValorUnidad('V'));
+        Assert.assertEquals(10, instance.retornaValorUnidad('X'));
+        Assert.assertEquals(50, instance.retornaValorUnidad('L'));
+        Assert.assertEquals(100, instance.retornaValorUnidad('C'));
+        Assert.assertEquals(500, instance.retornaValorUnidad('D'));
+        Assert.assertEquals(1000, instance.retornaValorUnidad('M'));
+        Assert.assertEquals(0, instance.retornaValorUnidad('J'));
 
     }
+
+    @Test(expected = Exception.class)
+    public void testCantidadAceptableCharException() throws Exception {
+        instance.convierte("CCCC");
+    }
+
+    @Test(expected = Exception.class)
+    public void testOrdenDescendienteException() throws Exception {
+        instance.convierte("XXL");
+    }
+
 
 }
